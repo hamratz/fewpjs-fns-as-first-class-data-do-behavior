5a0a059d36da8014882b81482c18403466a8db0e
Marty Warfel <marthawarfel@gmail.com>
2021-05-03 18:34:37 -0500

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,34 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+
+// CLICK(event listener) submit = 2 functions(greet(), displayMessage())
+//  greet()
+//    24 hour time, format HH:MM
+//    earlier > 12pm = "Good Morning"
+//    between < 12pm > 5pm = "Good Afternoon"
+//    later < 5pm = "Good Evening"
+//     return value = string
+//        convert string to number
+//          split()
+//          parseInt()
+//  displayMessage()
+//    one argument = string
+      // update text inside #greeting Node
+      // doesnt return anything
+
+
+// soloution has this first, not sure why
+function displayMessage(msg) {
+  document.getElementById("greeting").innerText = msg;
+}
+
+
+function greet(timeStr) {
+  const hour = parseInt(timeStr, 10);
+  if ( hour < 12) return "Good Morning"
+  if ( hour > 17) return "Good Evening"
+  return "Good Afternoon"
+}
+
